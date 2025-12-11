45e3b186b8a3f726184163280b2a810b20c5c7b9
Alexandra Shelukhina <sasha50395@yandex.ru>
2025-12-11 20:32:12 +0300

    тестирование

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,71 @@
+from http import HTTPStatus
+
+from django.urls import reverse
+
+from notes.models import Note
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур 
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+def test_note_exists(note):
+    notes_count = Note.objects.count()
+    # Общее количество заметок в БД равно 1.
+    assert notes_count == 1
+    # Заголовок объекта, полученного при помощи фикстуры note,
+    # совпадает с тем, что указан в фикстуре.
+    assert note.title == 'Заголовок'
+
+# Обозначаем, что тесту нужен доступ к БД. 
+# Без этой метки тест выдаст ошибку доступа к БД.
+@pytest.mark.django_db
+def test_empty_db():
+    notes_count = Note.objects.count()
+    # В пустой БД никаких заметок не будет:
+    assert notes_count == 0 
