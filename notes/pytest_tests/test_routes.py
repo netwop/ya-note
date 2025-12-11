df15941b1a6712bb3ac7f1b0a86e4068eef5974b
Alexandra Shelukhina <sasha50395@yandex.ru>
2025-12-11 20:32:53 +0300

    тестирование

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,16 +1,13 @@
+import pytest
+from pytest_django.asserts import assertRedirects
+
+from pytest_lazy_fixtures import lf
 from http import HTTPStatus
 
 from django.urls import reverse
 
 from notes.models import Note
 
-
-# Указываем в фикстурах встроенный клиент.
-def test_home_availability_for_anonymous_user(client):
-    # Адрес страницы получаем через reverse():
-    url = reverse('notes:home')
-    response = client.get(url)
-    assert response.status_code == HTTPStatus.OK
 
 @pytest.mark.parametrize(
     'name',  # Имя параметра функции.
@@ -30,18 +27,17 @@ def test_pages_availability_for_anonymous_user(client, name):
 def test_pages_availability_for_auth_user(not_author_client, name):
     url = reverse(name)
     response = not_author_client.get(url)
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.OK 
 
 
 @pytest.mark.parametrize(
     'parametrized_client, expected_status',
-    # Предварительно оборачиваем имена фикстур 
-    # в вызов функции pytest.lazy_fixture().
-    (
-        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
-        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
-    ),
+    [
+        (lf('not_author_client'), HTTPStatus.NOT_FOUND),
+        (lf('author_client'), HTTPStatus.OK)
+    ],
 )
+
 @pytest.mark.parametrize(
     'name',
     ('notes:detail', 'notes:edit', 'notes:delete'),
@@ -50,22 +46,28 @@ def test_pages_availability_for_different_users(
         parametrized_client, name, note, expected_status
 ):
     url = reverse(name, args=(note.slug,))
+    # Делаем запрос от имени клиента parametrized_client:
     response = parametrized_client.get(url)
+    # Ожидаем ответ страницы, указанный в expected_status:
     assert response.status_code == expected_status
 
 
-def test_note_exists(note):
-    notes_count = Note.objects.count()
-    # Общее количество заметок в БД равно 1.
-    assert notes_count == 1
-    # Заголовок объекта, полученного при помощи фикстуры note,
-    # совпадает с тем, что указан в фикстуре.
-    assert note.title == 'Заголовок'
-
-# Обозначаем, что тесту нужен доступ к БД. 
-# Без этой метки тест выдаст ошибку доступа к БД.
-@pytest.mark.django_db
-def test_empty_db():
-    notes_count = Note.objects.count()
-    # В пустой БД никаких заметок не будет:
-    assert notes_count == 0 
+@pytest.mark.parametrize(
+    'name, args',
+    [
+        ('notes:detail', lf('slug_for_args')),
+        ('notes:edit', lf('slug_for_args')),
+        ('notes:delete', lf('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ],
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url) 
